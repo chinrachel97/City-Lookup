5f944e19dfe3b3c9231cfd677a4120413574fecd
chinrachel97 <chinrachel97@gmail.com>
2018-10-29 17:41:55 -0500

    added comments for header

--- scrape.py
+++ scrape.py
@@ -34,25 +34,25 @@ for link in statesLinks:
 '''        
 
 # init the csv rows
-header = ["State",
-    "City",
-    "Population",
-    "Population Density",
-    "Population Category",
-    "Median Resident Age",
-    "Median Household Income",
-    "Unemployment Percentage",
-    "Median Rent",
-    "Cost of Living Index",
-    "Cost of Living Category",
-    "Travel Time to Work",
-    "Crime Index",
-    "Avg. Household Size",
-    "Air Quality Index",
-    "Poverty Percentage",
-    "Education Inequality Index"
+header = ["State",                  # State
+    "City",                         # City
+    "POP",                          # Population 
+    "POP Density (people/sqmile)",  # Population Density 
+    "POP Category",                 # Population Category
+    "Med. Res. Age (yrs)",          # Median Resident Age
+    "Med. HH Income ($)",           # Median Household Income
+    "Unemployment %",               # Unemployment Percentage
+    "Med. Rent ($)",                # Median Rent
+    "COL Index",                    # Cost of Living Index
+    "COL Category",                 # Cost of Living Category
+    "Mean Commute (mins)",          # Travel Time to Work
+    "Crime Index",                  # Crime Index
+    "Avg. HH Size (person)",        # Average Household Size
+    "Air Quality Index",            # Air Quality Index
+    "Poverty %",                    # Poverty Percentage
+    "Edu. Ineq. Index"              # Education Inequality Index
 ]
-toSave = []
+row = []
 
 # init the csv file
 filename = "city-data-scrape.csv"
@@ -80,8 +80,8 @@ r = "(\\w+), (\\w+)"
 m = re.search(r, fullName)
 stateName = m.group(2) 
 cityName = m.group(1)
-toSave.append(stateName)
-toSave.append(cityName)
+row.append(stateName)
+row.append(cityName)
 print("State:", stateName)
 print("City:", cityName)
 
@@ -89,7 +89,7 @@ print("City:", cityName)
 cityPopulationTag = soup.find('section', attrs={'id': 'city-population'})
 child = cityPopulationTag.find('b', recursive=False)
 population = child.nextSibling.strip()
-toSave.append(population)
+row.append(population)
 print("Population:", population)
 
 # get population density + category
@@ -99,20 +99,20 @@ populationDensity = None
 for child in children:
     if child.text == "Population density:":
         populationDensity = child.nextSibling.strip()
-toSave.append(populationDensity)
+row.append(populationDensity)
 print("Population Density: " + populationDensity + " people per square mile")
 
 populationDensityDeets = soup.find('span', attrs={'class': 'population-density'}).nextSibling
 r = "\\s*(?<=\\().+(?=\\))"
 densityDeets = re.search(r, populationDensityDeets).group(0).strip()
-toSave.append(densityDeets)
+row.append(densityDeets)
 print("Population Category: " + densityDeets)
 
 # get median resident age
 ageTag = soup.find('section', attrs={'id': 'median-age'})
 children = ageTag.select("div table tr td")[1].select("img")
 medianResidentAge = children[0].nextSibling.strip()
-toSave.append(medianResidentAge)
+row.append(medianResidentAge)
 print("Median Resident Age:", medianResidentAge)
 
 # get estimated median household income
@@ -121,53 +121,53 @@ children = incomeTag.select("b")
 medianIncome = children[0].nextSibling
 r = "\\$(.*)\\s\\("
 medianIncome = re.search(r, medianIncome).group(1)
-toSave.append(medianIncome)
+row.append(medianIncome)
 print("Median Household Income:", medianIncome)
 
 # get unemployment percentage
 unemploymentTag = soup.find('section', attrs={'id': 'unemployment'})
 children = unemploymentTag.select("div table tr td")[1].select("p")
 unemployment = children[0].nextSibling.strip()
-toSave.append(unemployment)
+row.append(unemployment)
 print("Unemployment Percentage:", unemployment)
 
 # get median rent
 rentTag = soup.find('section', attrs={'id': 'median-rent'})
 children = rentTag.select("p b")
 rent = children[0].nextSibling.strip()
-toSave.append(rent)
+row.append(rent)
 print("Median Rent:", rent)
 
 # get cost of living index + category
 costOfLivingTag = soup.find('section', attrs={'id': 'cost-of-living-index'})
 children = costOfLivingTag.select("b")
 costOfLiving = children[0].nextSibling.strip()
-toSave.append(costOfLiving)
+row.append(costOfLiving)
 print("Cost of Living Index:", costOfLiving)
 
 COLCategory = children[1].text
-toSave.append(COLCategory)
+row.append(COLCategory)
 print("Cost of Living Category:", COLCategory)
 
 # get median travel time to work
 travelTimeTag = soup.find('section', attrs={'id': 'education-info'})
 children = travelTimeTag.select("ul li")[4].select("b")
 travelTime = children[0].nextSibling.strip()
-toSave.append(travelTime)
+row.append(travelTime)
 print("Travel Time to Work:", travelTime)
 
 # get crime index, U.S. average = 280.5
 crimeTag = soup.find('section', attrs={'id': 'crime'})
 children = crimeTag.select("div table tr td")
 crimeIndex = children[len(children)-1].text.strip()
-toSave.append(crimeIndex)
+row.append(crimeIndex)
 print("Crime Index:", crimeIndex)
 
 # get average household size
 hSizeTag = soup.find('section', attrs={'id': 'households-stats'})
 children = hSizeTag.select("div div table tr td")[1].select("img")
 hSize = children[0].nextSibling.strip()
-toSave.append(hSize)
+row.append(hSize)
 print("Average Household Size:", hSize)
 
 # get median year house/condo built (SKIP; cannot find)
@@ -176,21 +176,21 @@ print("Average Household Size:", hSize)
 airQualityTag = soup.find('div', attrs={'id': 'air-pollution-chart'})
 children = airQualityTag.select("div div div table tr td")[1].select("p")
 airQualityIndex = children[0].nextSibling.strip()
-toSave.append(airQualityIndex)
+row.append(airQualityIndex)
 print("Air Quality Index:", airQualityIndex)
 
 # get poverty percentage
 povertyTag = soup.find('section', attrs={'id': 'poverty-level'})
 children = povertyTag.select("b")
 poverty = children[0].nextSibling.strip()
-toSave.append(poverty)
+row.append(poverty)
 print("Poverty Percentage", poverty)
 
 # get education gini index (inequality in education)
 giniTag = soup.find('section', attrs={'id': 'education-graphs'})
 children = giniTag.select("div div table tr td")[1].select("p")
 gini = children[0].nextSibling.strip()
-toSave.append(gini)
+row.append(gini)
 print("Inequality in Education Index:", gini)
 
 # end timing
@@ -200,4 +200,4 @@ print("Time taken:", elapsedTime)
 # write data to csv file
 with open('city-data-scrape.csv', 'a', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(toSave)
+    writer.writerow(row)
