8406971dc7907f4607748dfb48920d7f5873b13b
chinrachel97 <chinrachel97@gmail.com>
2018-11-17 20:07:37 -0600

    gets an array and calls py script with cmd args

--- getRecommendations.php
+++ getRecommendations.php
@@ -1,6 +1,8 @@
 <?php
   // call the python script to get recommendations
-  $command = escapeshellcmd('python test.py');
+  $input = $_POST["userInputs"];
+  $cmdArgs = implode(" ", $input);
+  $command = escapeshellcmd('python test.py '.$cmdArgs);
   $output = shell_exec($command);
   if(shell_exec($command)){
     echo $output;
