3393052e1ad95ccdc04b7c4a1c91205f61e30f4c
chinrachel97 <chinrachel97@gmail.com>
2018-11-17 21:51:53 -0600

    check for no data sent

--- getRecommendations.php
+++ getRecommendations.php
@@ -1,7 +1,13 @@
 <?php
+  $cmdArgs = "";
+
+  // check if there was any user input
+  if(isset($_POST["userInputs"])){
+    $input = $_POST["userInputs"];
+    $cmdArgs = implode(" ", $input);
+  }
+
   // call the python script to get recommendations
-  $input = $_POST["userInputs"];
-  $cmdArgs = implode(" ", $input);
   $command = escapeshellcmd('python test.py '.$cmdArgs);
   $output = shell_exec($command);
   if(shell_exec($command)){
