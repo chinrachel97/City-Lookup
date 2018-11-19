234fb6582f6f36cddddfe2520532ffab6491e687
chinrachel97 <chinrachel97@gmail.com>
2018-11-18 19:33:22 -0600

    fixed bug that calls py script without putting quotes around cmdline args

--- getRecommendations.php
+++ getRecommendations.php
@@ -4,6 +4,13 @@
   // check if there was any user input
   if(isset($_POST["userInputs"])){
     $input = $_POST["userInputs"];
+
+    // add double quotes around the inputs to preserve double words
+    for($i=0; $i<count($input); ++$i){
+      $input[$i] = "'".$input[$i]."'";
+    }
+
+    // join all the elements in the arr with a space
     $cmdArgs = implode(" ", $input);
   }
 
